53225829fd49ed13f70d9783d01e019f2d5522d8
Jens Ortner <jens.ortner@appliedtechnology.se>
2024-03-11 11:20:16 +0100

    added nav component

--- frontend/src/Components/Home.tsx
+++ frontend/src/Components/Home.tsx
@@ -1,15 +1,13 @@
 
 import './Home.css'
+import Nav from './Nav'
 
 function Home() {
   
 
   return (
     <>
-    <div className='home__logo-container'>
-    <img src="src/assets/logo.png" alt="src/assets/logo.png" width="400"/>
-    </div>
-
+    <Nav/>
     <div className='home__button-container'>
       <button>Become a VolunPair</button>
       <button>Find VolunPairs</button>
---
+++ frontend/src/Components/Nav.tsx
@@ -0,0 +1,11 @@
+import './Nav.css'
+function Nav () {
+
+    return (
+        <div className='nav__logo-container'>
+        <img src="src/assets/logo.png" alt="src/assets/logo.png" width="400"/>
+        </div>
+    )
+}
+
+export default Nav
