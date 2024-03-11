971c3fe1eb9893810943f36a748f5efa0ae308be
Jens Ortner <jens.ortner@appliedtechnology.se>
2024-03-11 11:45:43 +0100

    add onnclick event when pressing logo return to home

--- frontend/src/Components/Nav.tsx
+++ frontend/src/Components/Nav.tsx
@@ -1,8 +1,15 @@
+import Home from './Home'
 import './Nav.css'
-function Nav () {
 
+function Nav () {
+    
+
+  
+    const routeChange = () =>{ 
+     return(<Home/>)
+    }
     return (
-        <div className='nav__logo-container'>
+        <div className='nav__logo-container' onClick={() => routeChange}>
         <img src="src/assets/logo.png" alt="src/assets/logo.png" width="400"/>
         </div>
     )
