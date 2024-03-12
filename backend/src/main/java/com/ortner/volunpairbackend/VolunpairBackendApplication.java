f1a398a0d0fec242ee8ae715ee6911a2ce1321b2
Jens Ortner <jens.ortner@appliedtechnology.se>
2024-03-12 09:10:26 +0100

    added Spring project for backend

---
+++ backend/src/main/java/com/ortner/volunpairbackend/VolunpairBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ortner.volunpairbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunpairBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunpairBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ortner/volunpairbackend/VolunpairBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ortner.volunpairbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunpairBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
