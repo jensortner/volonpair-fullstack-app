8f26863d093712b9e960b562b1d4dad4dc53cbe8
Jens Ortner <jens.ortner@appliedtechnology.se>
2024-03-13 18:04:31 +0100

    changed features in navbar

--- frontend/src/Components/Nav.tsx
+++ frontend/src/Components/Nav.tsx
@@ -21,10 +21,20 @@ function Nav () {
       </ul>
     </div>
   </div>
-  <div className="navbar-center">
-  <Link className="btn btn-ghost text-xl" to="/">VolunPair</Link>
+  <div className="navbar-center text-xl">VolunPair</div>
+  <div className="navbar-end">
+  <Link to="/" className="menu menu-horizontal rounded-box">
+  <li>
+    <a>
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" 
+        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1
+         1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+    </a>
+  </li>
+  </Link>
   </div>
-</div>
+  </div>
       <Outlet /> 
         </>
 
