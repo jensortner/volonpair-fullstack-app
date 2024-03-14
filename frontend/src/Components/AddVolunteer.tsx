cc2d4315d97ae1cd84b5dd822d6cf8cee38dc848
Jens Ortner <jens.ortner@appliedtechnology.se>
2024-03-14 11:35:57 +0100

    fixe styling on navbar

--- frontend/src/Components/AddVolunteer.tsx
+++ frontend/src/Components/AddVolunteer.tsx
@@ -6,16 +6,65 @@ import './AddVolunteer.css'
 function AddVolunteer() {
     const [showErrorMsg, setShowErrorMsg] = useState<boolean>();
     const [showSuccessMsg, setShowSuccessMsg] = useState<boolean>();
-    const [name, setName ] = useState ('')
-    const [age, setAge ] = useState (0)
-    const [occupation, setOccupation ] = useState ('')
-    const [email, setEmail ] = useState ('')
-    const [city, setCity ] = useState ('')
     const imgUrl = 'https://avatar.iran.liara.run/public'
 
-   async function handleSubmit(event: any) {
-        event.preventDefault();
 
+
+    const [inputFields, setInputFields] = useState({
+      name: '', 
+      age: '',
+      occupation: '',
+      email: '',
+      city:''
+    });
+
+    const [errors, setErrors] = useState("");
+    const [submitting, setSubmitting] = useState(false);
+
+    const validateValues = (inputValues:any) => {
+    
+      if (inputValues.occupation === '' || inputValues.name === '' || inputValues.city === '') {
+        return "All fields are reqired";
+      }
+      if (inputValues.email.length < 15) {
+        return "Email is too short";
+      }
+      if (inputValues.age <= 0 || inputValues.age === null ) {
+        return "Age must be greater than 0";
+      }
+      
+      return ""
+    };
+
+    const handleChange = (name: string, value: string|number ) => {
+      setInputFields({ ...inputFields, [name]: value});
+    };
+    const handleSubmit = (event:any) => {
+      event.preventDefault();
+      setErrors(validateValues(inputFields));
+      setSubmitting(true);
+    };
+
+    const finishSubmit = () => {
+      handleFetch(inputFields)
+      setInputFields({
+        name: '', 
+        age: '',
+        occupation: '',
+        email: '',
+        city:''
+      })
+    };
+    
+    useEffect(() => {
+      if (Object.keys(errors).length === 0 && submitting) {
+        finishSubmit();
+      }
+    }, [errors]);
+    
+   async function handleFetch(inputFields:any) {
+      
+    
 
       await fetch('http://localhost:3000/api/volunteer', {
         method: 'POST',
@@ -23,11 +72,11 @@ function AddVolunteer() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          "name": name,
-          "age":  age,
-          "occupation": occupation,
-          "email": email,
-          "city": city,
+          "name": inputFields.name,
+          "age":  inputFields.age,
+          "occupation": inputFields.occupation,
+          "email": inputFields.email,
+          "city": inputFields.city,
           "imgUrl": imgUrl
         }),
       })
@@ -36,12 +85,6 @@ function AddVolunteer() {
           setShowSuccessMsg(true)
         })
         .catch(() => {setShowErrorMsg(true)})
-
-      setName('')
-      setAge(0)
-      setEmail('')
-      setOccupation('')
-      setCity('')  
     }
     useEffect(() =>{
         
@@ -59,71 +102,86 @@ function AddVolunteer() {
           
     },[showErrorMsg])
 
+    useEffect(() =>{
+        
+      setTimeout(() => {
+          setShowErrorMsg(false)
+        }, 3000); 
+        
+    },[showErrorMsg])
+
 
     return (
-
-    <div className="addvolunteer__form-container">
-    <form className="addvolunteer__form" onSubmit={handleSubmit}>
-      <input 
-        type="text" 
-        value={name}
-        placeholder="Full name"
-        className="input input-bordered w-full max-w-xs" 
-        onChange={(e) => setName(e.target.value)}
-         
-      />
+      <div className="addvolunteer__form-container">
+      <form className="addvolunteer__form" onSubmit={handleSubmit}>
         <input 
-          type="number" 
-          value={age}
-          placeholder="Age"
-          className="input input-bordered w-full max-w-xs"
-          onChange={(e) => setAge(e.target.valueAsNumber)} 
+          type="name" 
+          value={inputFields.name}
+          placeholder="Full name"
+          className="input input-bordered w-full max-w-xs" 
+          onChange={e => handleChange('name',e.target.value)}
+           
         />
-
-        <input 
-          type="text" 
-          value={email} 
-            placeholder="Email"
-            className="input input-bordered w-full max-w-xs" 
-            onChange={(e) => setEmail(e.target.value)} 
-        />
-
-        <input 
-          type="text" 
-          value={occupation}
-            placeholder="Occupation"
+          <input 
+            type="number" 
+            value={inputFields.age}
+            placeholder="Age"
             className="input input-bordered w-full max-w-xs"
-            onChange={(e) => setOccupation(e.target.value)}  
+            onChange={e => handleChange('age',e.target.value)} 
+          />
+          <input 
+            type="text" 
+            value={inputFields.email} 
+              placeholder="Email"
+              className="input input-bordered w-full max-w-xs" 
+              onChange={e => handleChange('email', e.target.value)} 
+          />
+  
+          <input 
+            type="text" 
+            value={inputFields.occupation}
+              placeholder="Occupation"
+              className="input input-bordered w-full max-w-xs"
+              onChange={e => handleChange('occupation',e.target.value)}  
+  
+          />
+  
+          <input 
+            type="text" 
+            value={inputFields.city}
+            placeholder="City"
+            className="input input-bordered w-full max-w-xs"
+            onChange={e => handleChange('city',e.target.value)}  
+          />
+          <div className='addvolunteer__form-button'>
+          <button  className="btn btn-wide" type="submit" > Add </button> 
+          </div>
+      </form>
+  
+          {showSuccessMsg && <div className='addvolunteer__form-messsage'><div role="alert" className="alert alert-success">
+          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" 
+          fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" 
+          strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <span>Your profile was sucessfully added!</span>
+          </div></div>}
+          
+          {showErrorMsg && <div className='addvolunteer__form-messsage'><div role="alert" className="alert alert-error">
+          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" 
+          fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" 
+          strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <span>Error! Failed to add.</span>
+          </div></div>}
 
-        />
+          {errors && <div className='addvolunteer__form-messsage'><div role="alert" className="alert alert-error">
+          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" 
+          fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" 
+          strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <span>{errors}</span>
+          </div></div>}
 
-        <input 
-          type="text" 
-          value={city}
-          placeholder="City"
-          className="input input-bordered w-full max-w-xs"
-          onChange={(e) => setCity(e.target.value)}  
-        />
-        <div className='addvolunteer__form-button'>
-        <input  className="btn btn-wide" type="submit" />
-        </div>
-    </form>
+      
+      </div>
 
-        {showSuccessMsg && <div className='addvolunteer__form-messsage'><div role="alert" className="alert alert-success">
-        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" 
-        fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" 
-        strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-        <span>Your profile was sucessfully added!</span>
-        </div></div>}
-        
-        {showErrorMsg && <div className='addvolunteer__form-messsage'><div role="alert" className="alert alert-error">
-        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" 
-        fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" 
-        strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-        <span>Error! Failed to add.</span>
-        </div></div>}
-    
-    </div>
         )
 }
 
--- frontend/src/Components/Nav.tsx
+++ frontend/src/Components/Nav.tsx
@@ -8,28 +8,25 @@ function Nav () {
     return (
         <>
         <div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
+          <Link to="/" className="navbar-start text-xl">
+          VolunPair
+          </Link>
+  <div className="navbar-end">
+    <div className="dropdown dropdown-bottom dropdown-end">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-box">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" 
         fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" 
         strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
       </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
         <li><Link to="/addvolunteer">Add Volunteer</Link></li>
         <li><Link to="/volunteers">Available Volunteers</Link></li>
       </ul>
     </div>
   </div>
-  <div className="navbar-center text-xl">VolunPair</div>
-  <div className="navbar-end">
-  <Link to="/" className="menu menu-horizontal rounded-box">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" 
-        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1
-         1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
-  </Link>
-  </div>
+
+      
+
   </div>
       <Outlet /> 
         </>
